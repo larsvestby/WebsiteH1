37627b08b6031894ad8eee4ba6323fc00d5f64cd
Lars Vestby <97159154+larsvestby@users.noreply.github.com>
2022-10-12 10:22:13 +0200

    Add files via upload

---
+++ H1_Website/JavaScript/filter.js
@@ -0,0 +1,63 @@
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+    }
+}
+
+//Her vises de filtrede elementer, ved hjælp af at tilføje klassen show.
+function AddClass(element, name) {
+    var i, arr1, arr2;
+
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+//Her gemmes de frafiltrede elementer, ved hjælp af at fjerne klassen show.
+function RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+//Denne funktion, skal tilføje klassen active til filterknappenerne. Og self også fjerne dem.
+function activeClass(e) {
+    var elems = document.querySelectorAll(".active");
+    [].forEach.call(elems, function (el) {
+        el.classList.remove("active");
+    });
+    e.target.className += " active";
+}
+
+//Dette er min søggefunktion.
+function myFunction() {
+    var input, filter, cards, cardContainer, h5, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("myItems");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-body p.hashtag span");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
